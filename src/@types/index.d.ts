c6b76fddf2f4bebbfc193e80ad6bf080babf8008
Chi Joshua <37910718+chijosh@users.noreply.github.com>
2019-11-18 00:17:26 +0200

    modified the circle ci config file, added test to header

---
+++ src/@types/index.d.ts
@@ -0,0 +1,7 @@
+declare module 'react-netlify-form' {}
+declare module 'gatsby-plugin-mailchimp' {}
+declare module 'gatsby-plugin-i18next'
+declare module '@storybook/addon-console'
+declare module '*.png'
+declare module '*.svg'
+declare module '*.jpg'
