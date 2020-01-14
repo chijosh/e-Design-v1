e1e11a90aa16bce993064723b2a7564bcc03f8f0
Chi Joshua <37910718+chijosh@users.noreply.github.com>
2020-01-14 04:05:36 +0200

    fixed git issues

--- src/components/directory/index.tsx
+++ src/components/directory/index.tsx
@@ -1,14 +1,8 @@
 import React from 'react'
-<<<<<<< Updated upstream
-=======
-import { graphql } from 'gatsby'
-import { FluidObject } from 'gatsby-image'
->>>>>>> Stashed changes
+import MenuItem from '../menu-item'
 
-// import MenuItem from '../menu-item'
 import './directory.scss'
 
-<<<<<<< Updated upstream
 export type SectionArr = Array<{
   title: string
   imageUrl: string
@@ -72,115 +66,6 @@ class Directory extends React.Component<SectionArr, State> {
       </div>
     )
   }
-=======
-// interface Sections {
-// 	title: string
-// 	imageUrl: string
-// 	size?: string
-// 	id: number
-// 	linkUrl: string
-// }
-
-// type SectionArr = Array<{ node: Sections }>
-
-// interface State {
-// 	sections: SectionArr
-// }
-
-interface myObject {
-	nodes: {
-		id: string
-		childImageSharp: {
-			fixed: FluidObject
-			fluid: FluidObject
-		}
-	}
-}
-
-// interface member {
-// 	title: string
-// 	imageUrl: string
-// 	size?: string
-// }
-
-interface Props {
-	data: {
-		imagesContainer: myObject
-	}
-}
-const Directory = ({ data }: Props) => {
-	console.log(data.imagesContainer)
-	// const imagesData: imageCont[] = data.allFile
-	// const [section, setSection] = useState<State>({
-	// 	sections: [
-	// {
-	// 	title: 'hats',
-	// 	imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-	// 	id: 1,
-	// 	linkUrl: 'shop/hats',
-	// },
-	// {
-	// 	title: 'jackets',
-	// 	imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-	// 	id: 2,
-	// 	linkUrl: 'shop/jackets',
-	// },
-	// {
-	// 	title: 'sneakers',
-	// 	imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-	// 	id: 3,
-	// 	linkUrl: 'shop/sneakers',
-	// },
-	// {
-	// 	title: 'womens',
-	// 	imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-	// 	size: 'large',
-	// 	id: 4,
-	// 	linkUrl: 'shop/womens',
-	// },
-	// {
-	// 	title: 'mens',
-	// 	imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-	// 	size: 'large',
-	// 	id: 5,
-	// 	linkUrl: 'shop/mens',
-	// },
-	// ],
-	// })
-
-	return (
-		// const renderImage = ()=>{
-		// 	return data.imagesData.map(({node})=>{
-		// 		const member = {
-
-		// 		}
-		// 	})
-		// }
-		<div className="directory-menu">
-			{/* {imagesData.map(({ title, imageUrl, size, id }: Props) => (
-				<MenuItem key={id} member={member} />
-			))} */}
-		</div>
-	)
->>>>>>> Stashed changes
 }
 
 export default Directory
-
-export const query = graphql`
-	query {
-		imagesContainer: allFile(filter: { relativeDirectory: { eq: "gallery" } }) {
-			nodes {
-				id
-				childImageSharp {
-					fixed {
-						...GatsbyImageSharpFixed
-					}
-					fluid {
-						...GatsbyImageSharpFluid
-					}
-				}
-			}
-		}
-	}
-`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,14 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/Layout'
 import HomePage from './HomePage'
 import SEO from '../components/Seo'
 
-<<<<<<< Updated upstream
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <HomePage />
   </Layout>
 )
-=======
-const IndexPage = () => {
-	return (
-		<Layout>
-			<SEO title="Home" />
-			<HomePage />
-			<Link to="/404">About</Link>
-		</Layout>
-	)
-}
->>>>>>> Stashed changes
 
 export default IndexPage
